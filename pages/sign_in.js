29676140675cd850df3dd00b0a2522ddbdb63db8
Guillaume Cristiani <guillaume.cristiani@epitech.eu>
2021-06-17 21:07:37 +0200

    feat: Login with error gestion and user feedback, no jwt

--- pages/sign_in.js
+++ pages/sign_in.js
@@ -1,34 +1,70 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-import Link from 'next/link'
+import styles from '../styles/sign_in.module.css'
+import Header from '../Components/Header/Header'
+import Router from 'next/router'
 
-export default function Home() {
-  return (
+function Sign_In() {
+	const onClickLogin = async event => {
+		document.getElementById('input_button').hidden = true;
+		document.getElementById('spinner').hidden = false;
+		event.preventDefault();
+		const form = {
+			username: event.target.input_id.value,
+			password: event.target.input_pass.value
+		}
+		await fetch(
+			'http://localhost:4000/auth/login', {
+			method: "POST",
+			headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=UTF-8'
+            },
+			body: new URLSearchParams(form)
+		})
+		.then( async (res) => {
+			if (res.status === 201) {
+				document.getElementById('spinner').hidden = true;
+				document.getElementById('correct').hidden = false;
+				await new Promise(r => setTimeout(r, 2000));
+				Router.push('/');
+			}
+			else {
+				document.getElementById('incorrect').hidden = false;
+				document.getElementById('spinner').hidden = true;
+				document.getElementById('input_button').hidden = false;	
+				document.getElementById('input_id').value = "";
+				document.getElementById('input_pass').value = "";
+			}
+		})
+		.catch((err) => {
+			console.log('ERROR: ' + err.status);
+		})
+	}
+	const onChangeLogin = async e => {
+		document.getElementById('incorrect').hidden = true;
+		document.getElementById('correct').hidden = true;
+		if ((document.getElementById('input_id').value !== "") && (document.getElementById('input_pass').value !== ""))
+			document.getElementById('input_button').disabled = false;
+		else
+			document.getElementById('input_button').disabled = true;
+	}
+  	return (
         <>
-        <header style={{backgroundColor: 'white'}}>
-          <div style={{ margin: '0% 1%', display: 'flex', flexDirection: 'row', alignItems: '', paddingBottom: '2%', paddingTop: '1%'}} >
-          <Link href="/">
-          <Image
-            src="/fort-rouge.png"
-            alt="logo"
-            width={70}
-            height={70}
-          />
-          </Link>
-          <p style={{margin: '1% 5%', fontFamily: 'system-ui', fontSize: '30px', }}>Fortrest</p>
-          </div>
-        </header>
-        <body style={{backgroundColor: '#C4C4C4'}}>
-          <form  style={{display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '10%'}}>
-            <label style={{paddingBottom: '1%'}} >Login</label>
-            <input id="name" type="text" autocomplete="name" placeholder='username' required style={{padding: '1% 2%', borderRadius: '5px', border: 'none', marginBottom: '1%'}}/>
-            <input id="name" type="password" autocomplete="name" placeholder='password' required style={{padding: '1% 2%', borderRadius: '5px', border: 'none'}}/>
-            <Link href="/central">
-                <button type="submit" style={{marginTop: '1%', backgroundColor: '#D8EAAB', fontFamily: 'system-ui', borderRadius: '5px', border: 'none', padding: '1% 1%'}} >Connexion</button>
-            </Link>
-        </form>
-        </body>
+        <Header />
+        <div style={{backgroundColor: '#C4C4C4'}}>
+			<form onSubmit={onClickLogin} onChange={onChangeLogin} style={{display: 'flex', flexDirection: 'column', alignItems: 'center', paddingTop: '10%'}}>
+				<label style={{paddingBottom: '1%', fontSize: '2.8vw'}} >Login</label>
+				<input id="input_id" type="text" autoComplete="name" placeholder='username' required style={{padding: '1% 2%', borderRadius: '5px', border: 'none', marginBottom: '1%'}}/>
+				<input id="input_pass" type="password" autoComplete="name" placeholder='password' required style={{padding: '1% 2%', borderRadius: '5px', border: 'none'}}/>
+				<button id="input_button" type="submit" disabled={true} style={{marginTop: '1%', backgroundColor: '#D8EAAB', fontFamily: 'system-ui', borderRadius: '5px', border: 'none', padding: '1% 1%'}} >Login</button>
+				<span id="incorrect" hidden={true} style={{color: 'red'}}>Login or password incorrect</span>
+				<span id="correct" hidden={true} style={{color: 'green'}}>Connected !</span>
+				<div id="spinner" className={styles.spinner} hidden={true}>
+					<div className={styles.double_bounce1}></div>
+					<div className={styles.double_bounce2}></div>
+				</div>
+			</form>
+        </div>
         </>
-      )
-    }
+	)
+}
+
+export default Sign_In
